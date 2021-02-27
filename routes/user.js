7bcbeeb6d3f315611ab040fff847b643f6d72782
ajaydevsysart <ajay@sysart.net>
2021-02-27 15:57:57 +0530

    stucked in password of undefined

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const producthelper = require('../helpers/product-helpers')
-
+const producthelper = require('../helpers/product-helpers');
+const userHelpers = require('../helpers/user-helpers');
 
 router.get('/', (req, res) => {
 
@@ -10,14 +10,25 @@ router.get('/', (req, res) => {
     })
 });
 
+//login get===================================
+
 router.get('/login',(req,res)=>{
     res.render('user/login',{admin:false})
 
 })
+
+//signup get===================================
 router.get('/signup',(req,res)=>{
     res.render('user/signup',{admin:false})
 
 })
+//signup post==================================
 
+router.post('/signup',(req,res)=>{
+    userHelpers.doSignup(req.body).then((response)=>{
+        console.log(response)
+    })
+
+})
 
 module.exports = router;
